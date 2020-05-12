da82a15a8eb5c6f5635247a60e5744ce6dc24ff1
cosmicfilament <john@thecosmicfilament.com>
2020-05-12 11:53:24 -0400

    Updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,6 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
+/**
+ * Checks if currentUser is set on the request object
+ * @param req 
+ * @param res 
+ * @param next 
+ * 
+ * @throws NotAuthorized Error
+ */
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
 	if (!req.currentUser) {
 		throw new NotAuthorizedError();
