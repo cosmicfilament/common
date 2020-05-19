f0e40990385be83577c7abbc221332f4b1057b98
cosmicfilament <john@thecosmicfilament.com>
2020-05-19 06:13:56 -0400

    Updates

--- build/middlewares/require-auth.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="qs" />
-import { Request, Response, NextFunction } from 'express';
-/**
- * Checks if currentUser is set on the request object
- *
- * @throws NotAuthorized Error which extends the base Error object
- */
-export declare const requireAuth: (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs>, res: Response<any>, next: NextFunction) => void;
